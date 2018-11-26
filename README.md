d646318e7e1c20e4379b98454384ba18301855d2
André Veríssimo <afsverissimo@gmail.com>
2018-11-26 18:01:24 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Download latest release from Atom
+
+Default settings download Linux 64-bits version of Atom and extracts to an existing directory `/opt/atom-wrapper/atom`.
+
+Directory must exist and user must have permissions to write to it.
+
+## Usage
+
+Edit `config.json` to change some configurations, such as the architecture to download
+or where to extract the contents.
+
+```
+npm start
+```
+
+That's it!
---
+++ index.js
@@ -0,0 +1,54 @@
+const octokit = require('@octokit/rest')();
+const tar = require('tar');
+const request = require('request');
+
+const opts = require('./config.json');
+
+// Retrieve information from github
+octokit
+  .repos
+  .listReleases({
+    owner: 'atom',
+    repo: 'atom',
+    per_page: 30,
+    page: 1
+  })
+  .then(response => {
+    // Sort releases by published date
+    const releases = response
+      .data
+      .sort((a, b) => a.published_at.localeCompare(b.publised_at));
+
+    // Get first
+    if (releases.length > 0 ) {
+      // Get amd64.tar.gz releases (should only be one)
+      const amd = releases[0]
+        .assets
+        .filter(el => new RegExp(opts.regex, opts.regexFlags).test(el.name));
+
+      if (amd.length > 0) {
+        console.log(`Downloading release with tag ${releases[0].tag_name} (prerelease: ${!!releases[0].prerelease})`);
+        console.log(`  Saving to directory: '${opts.path}'`);
+        console.log('');
+        // Download release and extract to path
+        request(amd[0].browser_download_url)
+          .on('error', error => {
+            console.log('ERROR: ', error);
+          })
+          .on('response', response => {
+            console.log('  Extracting...');
+            console.log('    note: this could take a while.');
+          })
+          .pipe(
+            tar.x({
+              strip: 1,
+              C: opts.path
+            })
+          )
+      } else {
+        console.log('NOTE: There is no amd64.tar.gz release');
+      }
+    } else {
+      console.log('NOTE: There are no releases');
+    }
+  });
