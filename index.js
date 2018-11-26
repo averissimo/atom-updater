59746ca9035e229078e0dd6f77bb611102f2d384
André Veríssimo <afsverissimo@gmail.com>
2018-11-26 18:27:14 +0000

    adds xo validation just for kicks

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* eslint camelcase: ["error", { allow: ["per_page"] }] */
+
 const octokit = require('@octokit/rest')();
 const tar = require('tar');
 const request = require('request');
@@ -20,14 +22,14 @@ octokit
       .sort((a, b) => a.published_at.localeCompare(b.publised_at));
 
     // Get first
-    if (releases.length > 0 ) {
+    if (releases.length > 0) {
       // Get amd64.tar.gz releases (should only be one)
       const amd = releases[0]
         .assets
         .filter(el => new RegExp(opts.regex, opts.regexFlags).test(el.name));
 
       if (amd.length > 0) {
-        console.log(`Downloading release with tag ${releases[0].tag_name} (prerelease: ${!!releases[0].prerelease})`);
+        console.log(`Downloading release with tag ${releases[0].tag_name} (prerelease: ${Boolean(releases[0].prerelease)})`);
         console.log(`  Saving to directory: '${opts.path}'`);
         console.log('');
         // Download release and extract to path
@@ -35,7 +37,7 @@ octokit
           .on('error', error => {
             console.log('ERROR: ', error);
           })
-          .on('response', response => {
+          .on('response', () => {
             console.log('  Extracting...');
             console.log('    note: this could take a while.');
           })
@@ -44,7 +46,7 @@ octokit
               strip: 1,
               C: opts.path
             })
-          )
+          );
       } else {
         console.log('NOTE: There is no amd64.tar.gz release');
       }
